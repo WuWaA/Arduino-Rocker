25a5e58b34f95f7746ec429090727ca67a08729f
王好球先辈 <wu.wa.a@qq.com>
2018-07-12 15:58:55 +0800

    update range algorithm

--- rocker/rocker.ino
+++ rocker/rocker.ino
@@ -3,11 +3,13 @@ int Y_PIN = A1;
 int X_MAX = 825;
 int X_MIN = 180;
 float X_CEN = 522;
-float X_RANG = X_MAX - X_MIN;
+float X_L_RANG = X_MAX - X_CEN;
+float X_R_RANG = X_CEN - X_MIN;
 int Y_MAX = 830;
 int Y_MIN = 190;
 float Y_CEN = 510;
-float Y_RANG = Y_MAX - Y_MIN;
+float Y_U_RANG = Y_MAX - Y_CEN;
+float Y_D_RANG = Y_CEN - Y_MIN;
 float X;
 float Y;
 float ANGLE = 0.00f;
@@ -18,23 +20,43 @@ void setup() {
 }
 
 void loop() {
-  X = -((analogRead(X_PIN) - X_CEN) / (X_RANG / 2)); // X -> -1 ~ 1
+  // X -> -1 ~ 1
+  float X_TMP = -(analogRead(X_PIN) - X_CEN);
+  if (X_TMP > 0) {
+    X = X_TMP / X_R_RANG;
+  }
+  else if (X_TMP < 0) {
+    X = X_TMP / X_L_RANG;
+  }
+  else {
+    X = 0.00f;
+  }
   
   // simple clear
   if (abs(X) < 0.02) {
     X = 0.00f;
   }
-  if (abs(X) > 0.95) {
+  if (abs(X) > 0.98) {
     X = 1.00f;
   }
   
-  Y = (analogRead(Y_PIN) - Y_CEN) / (Y_RANG / 2); // Y -> -1 ~ 1
-  
+  // Y -> -1 ~ 1
+  float Y_TMP = -(analogRead(Y_PIN) - Y_CEN);
+  if (Y_TMP > 0) {
+    Y = Y_TMP / Y_U_RANG;
+  }
+  else if (Y_TMP < 0) {
+    Y = Y_TMP / Y_D_RANG;
+  }
+  else {
+    Y = 0.00f;
+  }
+    
   // simple clear
   if (abs(Y) < 0.02) {
     Y = 0.00f;
   }
-  if (abs(Y) > 1.00) {
+  if (abs(Y) > 0.98) {
     Y = 1.00f;
   }
 
@@ -45,15 +67,12 @@ void loop() {
   if (LENGTH < 0.02) {
     LENGTH = 0.00f;
   }
-  if (LENGTH > 1.00) {
+  if (LENGTH > 0.98) {
     LENGTH = 1.00f;
   }
   
-  //Serial.print(analogRead(X_PIN));
   Serial.print(LENGTH);
   Serial.print(",");
-  //Serial.print(analogRead(Y_PIN));
-  //Serial.print(analogRead(X_PIN) - X_CEN);
   Serial.print(ANGLE);
   Serial.println();
 }
